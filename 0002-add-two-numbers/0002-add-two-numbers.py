2814fa66b2f7e218b827e3e36603cd4e5c8bebfb
JK.Min <75975464+calendar3450@users.noreply.github.com>
2025-05-14 06:10:13 +0900

    Time: 7 ms (32.23%), Space: 18 MB (35.88%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,46 +5,40 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        result_l1=[]
-        result_l2=[]
-        result = []
+        result1 =[]
+        result2=[]
 
-        def listNode_l1(node):
+        def listNode1(node):
+            if node ==None:
+                return
+            result1.append(node.val)
+            listNode1(node.next)
+
+        def listNode2(node):
             if node==None:
-                return result_l1
-            result_l1.append(node.val)
-            listNode_l1(node.next)
+                return
+            result2.append(node.val)
+            listNode2(node.next)
+
+        listNode1(l1)
+        listNode2(l2)
+        result1Reverse = ''
+        result2Reverse = ''
         
-        def listNode_l2(node):
-            if node==None:
-                return result_l2
-            result_l2.append(node.val)
-            listNode_l2(node.next)
+        for i in range(len(result1)-1,-1,-1):
+            result1Reverse +=str(result1[i])
+        for i in range(len(result2)-1,-1,-1):
+            result2Reverse +=str(result2[i])
 
-        listNode_l1(l1)
-        listNode_l2(l2)
+        resultReverse = int(result1Reverse) +int(result2Reverse)
+        resultReverseList= list(str(resultReverse))
+        dummy =ListNode(0)
+        current = dummy
 
-        l1_str_reverse =''
-        l2_str_reverse=''
+        for i in range(len(resultReverseList)-1,-1,-1):
+            current.next= ListNode(int(resultReverseList[i]))
+            current = current.next
 
-        for i in range(len(result_l1)-1,-1,-1):
-            l1_str_reverse+=str(result_l1[i])
-        for i in range(len(result_l2)-1,-1,-1):
-            l2_str_reverse+=str(result_l2[i])
+        return dummy.next
 
-        l1_int_reverse = int(l1_str_reverse)
-        l2_int_reverse = int(l2_str_reverse)
-
-        result_int = l1_int_reverse + l2_int_reverse
-
-        for i in range(len(str(result_int))-1,-1,-1):
-            result.append(str(result_int)[i])
-
-        head = ListNode(int(result[0]))
-        current_node = head
-
-        for i in range(1,len(result)):
-            current_node.next = ListNode(int(result[i]))
-            current_node = current_node.next
-
-        return head
+        
