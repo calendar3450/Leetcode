1563b5fcfcf92ea26c0a19f1015a269076744461
JK.Min <75975464+calendar3450@users.noreply.github.com>
2025-12-04 12:33:29 +0900

    Time: 7 ms (42.99%), Space: 17.6 MB (89.9%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,45 +1,54 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        result = 0
+        total = 0
+
         for i in range(len(s)-1):
             if s[i] == 'I':
-                if s[i+1] == 'V' or s[i+1]=='X':
-                    result -=1
+                if s[i+1] =='V' or  s[i+1] =='X':
+                    total -=1
                 else:
-                    result +=1
-                
-            elif s[i] == 'V':
-                result += 5
-            elif s[i] == 'X':
-                if s[i+1]=='C' or s[i+1] =='L':
-                    result -=10
-                else:
-                    result += 10
-            elif s[i] == 'L':
-                result +=50
-            elif s[i] == 'C':
-                if s[i+1] == 'M' or s[i+1]=='D':
-                    result -=100
-                else:
-                    result +=100
-            elif s[i] == 'D':
-                result += 500
-            elif s[i] == 'M':
-                result +=1000
+                    total +=1
 
+            elif s[i] == 'V':
+                total +=5
+
+            elif s[i] == 'X':
+                if  s[i+1] =='L' or  s[i+1] =='C':
+                    total -= 10
+                else:
+                    total +=10
+
+            elif s[i] == 'L':
+                total +=50
+
+            elif s[i] == 'C':
+                if  s[i+1] =='D' or  s[i+1] =='M':
+                    total -=100
+                else:
+                    total +=100
+            elif s[i] == 'D':
+                total += 500
+
+            elif s[i] == 'M':
+                total +=1000
+        
         if s[-1] == 'I':
-            result +=1
+            total +=1
+
         elif s[-1] == 'V':
-            result += 5
+            total +=5
+
         elif s[-1] == 'X':
-            result += 10
+            total +=10
+
         elif s[-1] == 'L':
-            result +=50
+            total +=50
+
         elif s[-1] == 'C':
-            result +=100
+            total +=100
         elif s[-1] == 'D':
-            result += 500
+            total += 500
         elif s[-1] == 'M':
-            result +=1000
-            
-        return result
+            total +=1000
+
+        return total
