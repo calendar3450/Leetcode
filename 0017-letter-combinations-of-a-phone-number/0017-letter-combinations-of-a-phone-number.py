2a7edc7dc43b39a38f0eb7ac3228bb2d7827583a
JK.Min <75975464+calendar3450@users.noreply.github.com>
2025-12-07 13:29:53 +0900

    Time: 0 ms (100%), Space: 18 MB (21.93%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,27 +1,17 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if len(digits) == 0:
-            return []
-
-        mappings ={
-            '2':'abc',
-            '3':'def',
-            '4':'ghi',
-            '5':'jkl',
-            '6':'mno',
-            '7':'pqrs',
-            '8':'tuv',
-            '9':'wxyz'
-        }
         result = []
-        def backtracking(index,char):
-            if len(digits) == index:
-                result.append(char)
+        strs = {'2':'abc', '3':'def', '4':'ghi', '5':'jkl', '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
+        
+        def backtracking(index, path):
+            if index == len(digits):
+                result.append(path)
                 return
 
-            for i in mappings[digits[index]]:
-                backtracking(index+1,char+i)
+            for char in strs[digits[index]]:
+                backtracking(index +1, path + char)
         
         backtracking(0,'')
+
 
         return result
