d1dbd44265dbdd7a4ff2181d38989b9e7b18c0ef
JK.Min <75975464+calendar3450@users.noreply.github.com>
2025-12-23 11:06:26 +0900

    Time: 1944 ms (5.11%), Space: 20 MB (98.09%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,11 +1,31 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        result=collections.defaultdict(list)
+        n= len(strs)
+        result = []
+        tmp = []
+        
 
         for i in strs:
-            result[''.join(sorted(i))].append(i)
-        return list(result.values())
+            sort_i = sorted(list(i))
+            sort_j = ''
+            for j in sort_i:
+                sort_j +=j
+            tmp.append(sort_j)
 
-                
-            
+        visited = [False] * n
+
+        for i in range(n):
+            if visited[i]:
+                continue
+
+            tmp2 =[strs[i]]
+            visited[i] = True
+
+            for j in range(i+1,n):
+                if not visited[j] and tmp[i] == tmp[j]:
+                    tmp2.append(strs[j])
+                    visited[j] = True
+
+            result.append(tmp2)
         
+        return result
