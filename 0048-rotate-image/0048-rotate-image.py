153af1deebe936243ac1c8a85efd691acb628fae
JK.Min <75975464+calendar3450@users.noreply.github.com>
2025-12-23 10:07:18 +0900

    Time: 0 ms (100%), Space: 17.4 MB (96.52%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -4,9 +4,10 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         n = len(matrix)
-        tmp = [row[:] for row in matrix]
-
-        for i in range(n):
-            for j in range(n):
-                matrix[i][j] = tmp[n-1-j][i]
+        matrix2 = matrix.copy()
         
+        for c in range(n):
+            tmp = []
+            for r in range(n-1,-1,-1):
+                tmp.append(matrix2[r][c])
+            matrix[c] = tmp
