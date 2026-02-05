7166acb3e5990439d5dbdb4f567fe8ef8a5bc974
JK.Min <75975464+calendar3450@users.noreply.github.com>
2026-02-05 14:38:02 +0900

    Time: 0 ms (100%), Space: 19.7 MB (11.35%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -6,19 +6,29 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        if not root.left and not root.right:
+        rightSide=  root.right
+        leftSide = root.left
+        checking = []
+        
+        def DFS(leftNode, rightNode):
+            if not leftNode and not rightNode:
+                return
+            elif not leftNode and rightNode:
+                checking.append(False)
+                return
+            elif leftNode and not rightNode:
+                checking.append(False)
+                return
+        
+            if leftNode.val == rightNode.val:
+                DFS(leftNode.right, rightNode.left)
+                DFS(leftNode.left, rightNode.right)
+            else:
+                checking.append(False)
+
+        DFS(leftSide,rightSide)
+
+        if checking:
+            return False
+        else:
             return True
-
-        queue = deque([(root.left,root.right)])
-        while queue:
-            left,right = queue.popleft()
-            
-            if not left and not right:
-                continue
-            if not left or not right or left.val != right.val:
-                return False
-
-            queue.append((left.left,right.right))
-            queue.append((left.right,right.left))
-
-        return True
