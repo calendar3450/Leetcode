24546299a2872c256799b3d57139a58a9e9c218d
JK.Min <75975464+calendar3450@users.noreply.github.com>
2026-02-27 16:33:38 +0900

    Time: 0 ms (100%), Space: 19.5 MB (6.92%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,12 +1,11 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        a = sorted(nums,key=lambda x : str(x)*9,reverse=True)
-        ans = ''
-
-        for i in a:
-            ans+=str(i)
-
-        if int(ans)==0:
-            return '0'
-        else:
-            return ans
+        nums.sort(key =lambda x: str(x) *9, reverse=True)
+        answer = ''
+        for i in nums:
+            answer +=str(i)
+        
+        if int(answer) == 0:
+            answer = '0'
+        
+        return answer
