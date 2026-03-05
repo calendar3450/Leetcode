34ced0e0c3041eff55a54b5c98448855bf1af90e
JK.Min <75975464+calendar3450@users.noreply.github.com>
2026-03-05 15:22:31 +0900

    Time: 3 ms (54.09%), Space: 19.4 MB (88.1%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,17 +1,18 @@
+from collections import Counter
+
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        result = []
-        dic = {}
-        for i in words:
-            if i in dic:
-                dic[i] +=1
-            else:
-                dic[i] =1
+        ans = []
+        tmp = []
+        dict_a = Counter(words)
+        sorted_dict = sorted(dict_a.items(), key = lambda item: item[1], reverse = True)
         
-        d1 = sorted(dic.items(), key=operator.itemgetter(1),reverse = True)
+        for i in range(len(dict_a)):
+            tmp.append([sorted_dict[i][0],sorted_dict[i][1],words.index(sorted_dict[i][0])])
 
-        for i,word in enumerate(d1):
-            if i<k:
-                result.append(d1[i][0])
+        tmp.sort(key= lambda x: (-x[1],x[0]))
+        
+        for i in range(k):
+            ans.append(tmp[i][0])
 
-        return result
+        return ans
