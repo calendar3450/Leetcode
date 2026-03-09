efee30298c0f78f4f1d96221c66db6f09fc8735b
JK.Min <75975464+calendar3450@users.noreply.github.com>
2026-03-09 13:20:40 +0900

    Time: 319 ms (5.11%), Space: 25.9 MB (99.76%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -0,0 +1,25 @@
+class Trie:
+
+    def __init__(self):
+        self.words = []
+
+    def insert(self, word: str) -> None:
+        self.words.append(word)
+
+    def search(self, word: str) -> bool:
+        return word in self.words
+
+    def startsWith(self, prefix: str) -> bool:
+        compare_word = ""
+        for word in self.words:
+            if word.startswith(prefix):  # 시작 여부 체크로 수정
+                return True
+        
+        return False
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
