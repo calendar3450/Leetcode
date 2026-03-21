9b67e5751fe97fc6e87012ff3366c5eed311495b
JK.Min <75975464+calendar3450@users.noreply.github.com>
2026-03-21 21:40:02 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,25 +1,46 @@
+from collections import deque
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows,cols = len(grid),len(grid[0])
-        result = 0
-
-        def dfs(x,y):
-            if x<0 or x>=rows or y<0 or y>=cols or grid[x][y] =='0':
-                return 
-
-            grid[x][y] ='0'
-
-            dfs(x+1,y)
-            dfs(x-1,y)
-            dfs(x,y+1)
-            dfs(x,y-1)
-
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] =='1':
-                    result+=1
-                    dfs(i,j)
-
-        return result
-
+        n = len(grid)
+        m = len(grid[0])
+        ans = 0
+        
+        # def BFS(x,y):
+        #     queue = deque()
+        #     queue.append((x,y))
+        #     grid[x][y] ="0"
             
+        #     while queue:
+        #         x,y = queue.popleft()
+        #         if grid[x][y] != "0":
+        #             if 0<=x+1<m:
+        #                 queue.append((x+1,y))
+        #             if 0<=x-1<m:
+        #                 queue.append((x-1,y))
+        #             if 0<=y+1<n:
+        #                 queue.append((x,y+1))
+        #             if 0<=y-1<n:
+        #                 queue.append((x,y-1))
+
+        def BFS(x, y):
+            queue = deque()
+            queue.append((x, y))
+            grid[x][y] = "0"
+            
+            while queue:
+                x, y = queue.popleft()
+                
+                for dx, dy in [(1,0),(-1,0),(0,1),(0,-1)]:
+                    nx, ny = x+dx, y+dy
+                    if 0<=nx<n and 0<=ny<m and grid[nx][ny] == "1":  # 범위 + '1' 체크
+                        grid[nx][ny] = "0"   # 즉시 방문처리
+                        queue.append((nx, ny))
+
+        for y in range(m):
+            for x in range(n):
+                if grid[x][y] == "1":
+                    BFS(x,y)
+                    ans +=1
+
+        return ans
