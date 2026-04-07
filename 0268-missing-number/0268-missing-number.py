97c0effd5677c74769721e633120c0feb459a940
JK.Min <75975464+calendar3450@users.noreply.github.com>
2026-04-07 23:22:02 +0900

    Time: 7 ms (34.8%), Space: 20.4 MB (84.15%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        for idx,num in enumerate(nums):
+            if idx != num:
+                return idx
+        return len(nums)
