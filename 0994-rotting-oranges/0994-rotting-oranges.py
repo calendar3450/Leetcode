9e42189170d576e88d89986c72b261d303d34dd8
JK.Min <75975464+calendar3450@users.noreply.github.com>
2026-06-22 23:43:58 +0900

    Time: 6 ms (20.2%), Space: 19.2 MB (78.62%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -2,32 +2,32 @@ from collections import deque
 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
+        row = len(grid)
+        col = len(grid[0])
+        dir = [(0,1),(0,-1),(1,0),(-1,0)]
         queue = deque()
         fresh = 0
 
-        for i in range(m):
-            for j in range(n):
+        for i in range(row):
+            for j in range(col):
+
                 if grid[i][j] == 2:
                     queue.append((i,j))
 
                 elif grid[i][j] == 1:
-                    fresh +=1
-
-        time = 0 
-        
-        directions = [(1,0),(-1,0),(0,1),(0,-1)]
+                    fresh+=1
+        time = 0
 
         while queue and fresh >0:
             for _ in range(len(queue)):
-                i,j = queue.popleft()
-                for di, dj in directions:
-                    ni,nj = i + di, j + dj
-
-                    if 0<= ni < m and 0 <= nj < n and grid[ni][nj] ==1:
-                        grid[ni][nj] =2
-                        fresh -=1
-                        queue.append((ni,nj))
+                x,y = queue.popleft()
+                for dx,dy in dir:
+                    nx = x + dx
+                    ny = y + dy
+                    if 0<=nx<row and 0<=ny<col and grid[nx][ny] ==1:
+                        grid[nx][ny] = 2
+                        fresh-=1
+                        queue.append((nx,ny))
             time +=1
 
         return time if fresh == 0 else -1
